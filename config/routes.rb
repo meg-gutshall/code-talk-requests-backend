004ca2a363b21652b1042c4566f2c50c823642c5
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 23:12:00 -0400

    Add login and profile routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       # TODO: Adjust paths to remove "api/v1/"
       resources :users, only: [:index, :create, :show]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
 end
