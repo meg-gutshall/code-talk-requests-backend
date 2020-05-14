10508b140255af19bb305f5b2e6ff8c52657097f
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 21:31:54 -0400

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      # TODO: Adjust paths to remove "api/v1/"
+      resources :users, only: :index
+    end
+  end
 end
