b858cfe520f02cbaee95f17eb487e0eea6633aa2
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 23:09:10 -0400

    Add user create action and route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,29 +1,32 @@
 class Api::V1::UsersController < ApplicationController
+  # TODO: Look into the JSON output formatting
+
   def index
     users = User.all
-    render json: UserSerializer.new(users)
+    render json: { users: UserSerializer.new(users) }
   end
   
-  # def create
-  #   user = User.new(user_params)
-  #   if user.save
-  #     render json: user, status: :accepted
-  #   else
-  #     render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
-  #   end
-  # end
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { user: UserSerializer.new(user) }, status: :accepted
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
 
   def show
     user = User.find(params[:id])
     if user
-      render json: UserSerializer.new(user), status: :accepted
+      render json: { user: UserSerializer.new(user) }, status: :accepted
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
     end
   end
   
   private
+    # TODO: Check into whether require(:user) is need in the user_params method
     def user_params
-      params.permit(:first_name, :last_name, :email_address, :password_digest, :role)
+      params.require(:user).permit(:first_name, :last_name, :email_address, :password_digest, :role)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # TODO: Adjust paths to remove "api/v1/"
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :create, :show]
     end
   end
 end
