81fb6664e25919fc8966154d76af859582af99dd
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 22:11:38 -0400

    Add user index action and strong params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+  
+  # def create
+  #   user = User.new(user_params)
+  #   if user.save
+  #     render json: user, status: :accepted
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
+  #   end
+  # end
+  
+  private
+    def user_params
+      params.permit(:first_name, :last_name, :email_address, :password_digest, :role)
+    end
 end
