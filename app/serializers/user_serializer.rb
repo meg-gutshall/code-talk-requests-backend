4af7ee258e5f321569da60b3b26a009c15154c42
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 22:52:59 -0400

    Remove password_digest from user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :email_address, :password_digest, :role
+  attributes :first_name, :last_name, :email_address, :role
 end
