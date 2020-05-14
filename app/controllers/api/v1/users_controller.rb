a6af7cc31c6002cb507bca7d2924b233a5b2f9fd
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-13 22:24:35 -0400

    Add user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def index
     users = User.all
-    render json: users
+    render json: UserSerializer.new(users)
   end
   
   # def create
