2c36cf85c392d7ef7017fa38984c65c1de38bf33
Meg Gutshall <meghan.gutshall@gmail.com>
2020-05-21 07:36:07 -0400

    Delete user profile route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       # TODO: Adjust paths to remove "api/v1/" ??
       resources :users, only: [:index, :create, :show]
       post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
     end
   end
 end
