eafbf406b73bfec5344ae0f76bdccdcbfcd901e9
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-14 18:01:38 -0400

    Create request#index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
       post '/login', to: 'auth#create'
+      resources :users, only: [:index, :show, :create]
+      resources :requests, only: [:index]
     end
   end
 end
