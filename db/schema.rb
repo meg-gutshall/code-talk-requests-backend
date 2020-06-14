6c979e539e118f7fc029c4c43da0e9e30681e4de
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-14 17:55:51 -0400

    Create Request seeds and run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_011647) do
+ActiveRecord::Schema.define(version: 2020_06_14_212349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "requests", force: :cascade do |t|
+    t.string "topic"
+    t.string "module"
+    t.text "description"
+    t.bigint "student_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_id"], name: "index_requests_on_student_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values. The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 User.destroy_all
+Request.destroy_all
 
 # Create users
 meg = User.create!(
@@ -31,4 +32,15 @@ nick = User.create!(
   password: "password",
   role: "student"
 )
+puts "Created #{User.student.count} students"
+
+# Create requests
+Faker::Number.between(from: 6, to: 14).times do
+  Request.create!(
+    topic: Faker::Lorem.paragraph_by_chars(number: 50, supplemental: true),
+    module: Faker::Lorem.sentence(word_count: 1, supplemental: true, random_words_to_add: 2),
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 1),
+    student: User.student.sample
+  )
+end
 puts "Created #{User.all.count} users"
