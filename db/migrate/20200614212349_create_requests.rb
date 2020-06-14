1cd247a7c947fef61cd1fd51ab9fdc09d30c3cf4
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-14 17:31:31 -0400

    Create Requests migration file

--- db/migrate/20200614212349_create_requests.rb
+++ db/migrate/20200614212349_create_requests.rb
@@ -1,8 +1,13 @@
 class CreateRequests < ActiveRecord::Migration[6.0]
   def change
     create_table :requests do |t|
+      t.string :topic
+      t.string :module
+      # t.string :section
+      t.text :description
+      t.references :student
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
