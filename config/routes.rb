29647d5377e95e258a9f75410b8430e7e7eda1a3
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-14 15:10:40 -0400

    Remove code comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # TODO: Adjust paths to remove "api/v1/" ??
       resources :users, only: [:index, :create, :show]
       post '/login', to: 'auth#create'
     end
