c2ff2c07bcd681027ef78cb25f2983d08e6ad553
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-18 17:29:39 -0400

    Comment in attr_accessors

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ApplicationRecord
+  # attr_accessor :topic, :module, :description, :student_id
   belongs_to :student, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # attr_accessor :first_name, :last_name, :email_address, :password
   has_secure_password
 
   has_many :requests, foreign_key: :student_id
