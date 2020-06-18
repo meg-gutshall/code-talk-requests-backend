7d3aeece7e2b1d5839b09560e0e62d43c9eeff8c
Meg Gutshall <meghan.gutshall@gmail.com>
2020-06-18 17:41:52 -0400

    Fix serializer associations

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -2,5 +2,5 @@ class RequestSerializer
   include FastJsonapi::ObjectSerializer
   set_id :student_id
   attributes :topic, :module, :description, :student_id
-  belongs_to :student, serializer: :user
+  belongs_to :student, record_type: :user
 end
