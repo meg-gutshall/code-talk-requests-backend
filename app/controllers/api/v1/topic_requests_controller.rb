ba2f1818bafcf87a2c97b09691b8f37b8a28e605
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-01 12:58:36 -0400

    Add private #topic_request_params method to controller

--- app/controllers/api/v1/topic_requests_controller.rb
+++ app/controllers/api/v1/topic_requests_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::TopicRequestsController < ApplicationController
     topic_requests = TopicRequest.all
     render json: TopicRequestSerializer.new(topic_requests)
   end
+
+  private
+  def topic_request_params
+    params.permit(:topic, :module, :description, :upvotes, :student_id)
+  end
 end
