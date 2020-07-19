b90cde50a27eb8b7cb021de9858e058d183d60d1
Meg Gutshall <meghan.gutshall@gmail.com>
2020-07-19 19:57:01 -0400

    Add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'auth#create'
+      post '/logout', to: 'auth#destroy'
       resources :users, only: [:show, :create]
       resources :topic_requests, only: [:index]
     end
