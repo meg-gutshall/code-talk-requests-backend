538c3065b6c1cb05088f74099c3068c387857447
Meg Gutshall <meghan.gutshall@gmail.com>
2020-10-28 23:40:02 -0400

    Make TopicRequests#create action

--- app/controllers/api/v1/topic_requests_controller.rb
+++ app/controllers/api/v1/topic_requests_controller.rb
@@ -4,6 +4,15 @@ class Api::V1::TopicRequestsController < ApplicationController
     render json: TopicRequestSerializer.new(topic_requests)
   end
 
+  def create
+    topic_request = TopicRequest.new(topic_request_params)
+    if topic_request.save
+      render json: { topic_request: TopicRequestSerializer.new(topic_request) }, status: :created
+    else
+      render json: { errors: topic_request.errors.full_messages }, status: :not_acceptable
+    end
+  end
+
   private
   def topic_request_params
     params.require(:topic_request).permit(:idea, :description, :upvotes, :codepanion_id)
